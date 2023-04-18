bceb12e8b57941940791d9a399b257210067fc37
bkarakadioglu <batuhankarakadioglu@gmail.com>
2023-04-19 00:09:16 +0300

    Add memleket data

--- apps/coordinated-trends/pythonstuff/ittifakAdder.py
+++ apps/coordinated-trends/pythonstuff/ittifakAdder.py
@@ -1,13 +1,13 @@
 import json
 
 # load the JSON file into a Python variable
-with open('apps\coordinated-trends\pythonstuff\millet181.json', 'r') as f:
+with open('apps\coordinated-trends\pythonstuff\memleket30.json', 'r') as f:
     data = json.load(f)
 
 # add a new key-value pair to each item in the JSON object
 for item in data:
-    item['ittifak'] = 0
+    item['ittifak'] = 2
 
 # save the modified JSON object back to the file
-with open('apps\coordinated-trends\pythonstuff\ewmillet.json', 'w') as f:
+with open('apps\coordinated-trends\pythonstuff\ewmemleket.json', 'w') as f:
     json.dump(data, f)
